762141d4c6539df195969921ce2e283069ee4da8
Adityammar65 <adityaammar65@gmail.com>
2026-07-13 18:49:39 +0700

    fix some errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { base } from "framer-motion/client";
 
 // https://vite.dev/config/
 export default defineConfig({
