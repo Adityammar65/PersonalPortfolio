cfdae11545ea9cf42e629bf41ec6b271ff1e49e0
Adityammar65 <adityaammar65@gmail.com>
2026-07-13 18:47:46 +0700

    fix some errors

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
 import { Home, ProjectDetail } from "../pages";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <MainLayout />,
